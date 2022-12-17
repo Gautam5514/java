6004f87a5d4f258341c33f380c0c491dd072dce9
Gautam <gautampandit797@gmail.com>
2022-12-17 11:12:52 +0530

    initial commit

---
+++ literal.java
@@ -0,0 +1,14 @@
+public class literal {
+    public static void main(String[]ar){
+        int a = 123;
+
+        int b = 0123;
+
+        int c = 0x123;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+    
+}
---
+++ produt.java
@@ -0,0 +1,10 @@
+public class produt {
+    public static void main(String []args){
+
+        int x = 10;
+        int y = 20;
+        int product (x*y);
+        System.out.println()
+    }
+    
+}
